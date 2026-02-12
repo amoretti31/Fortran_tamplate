70868d37a8ecba788120fe3c9e86c808136485e7
Andrea Moretti <a.moretti31@studenti.unipi.it>
2026-02-12 23:45:13 +0100

    new test out

--- Docker/build_test.sh
+++ Docker/build_test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Creating build directory..."
+
 mkdir -p build
 cd build
 
-echo "Compiling module..."
 gfortran -Wall -Wextra -std=f2008 \
     -c ../src/module.f90
 
-echo "Compiling tests..."
 gfortran -Wall -Wextra -std=f2008 \
     ../tests/test_math.f90 module.o \
     -o test_math
@@ -17,7 +15,6 @@ gfortran -Wall -Wextra -std=f2008 \
 echo "Running tests..."
 ./test_math
 
-echo "Tests finished successfully"
 
 cd ..
 rm -r build
